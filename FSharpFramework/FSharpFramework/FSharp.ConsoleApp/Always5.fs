6eba8dbbd4135c7500e75ff6d22958f67e32af19
Danny Warren <dannydwarren@gmail.com>
2017-06-07 14:17:01 -0600

    Add non-xamarin flavor of impl

---
+++ FSharpFramework/FSharpFramework/FSharp.ConsoleApp/Always5.fs
@@ -0,0 +1,59 @@
+﻿module Always5 
+    module Trick1 =
+        let AddNextHighestNumber current = current + (current + 1)
+        let Add9 current = current + 9
+        let DivideBy2 current = current / 2
+        let SubtractOriginal (current, original) = current - original
+
+        let AddNextHighestNumberAdd9DivideBy2 =
+            AddNextHighestNumber >> Add9 >> DivideBy2
+
+        let AddNextHighestNumberAdd9DivideBy2Pipe x =
+            x |> AddNextHighestNumber |> Add9 |> DivideBy2
+
+        let Solve anyNumber:int = 
+            anyNumber
+            |> AddNextHighestNumberAdd9DivideBy2
+            |> fun x-> (x, anyNumber)
+            |> SubtractOriginal 
+    
+    module Trick2 =
+        type AnyNumber1To99 = private AnyNumber1To99 of int
+                
+        
+        let CreateAnyNumber1To99FromInt (aNumber:int) =
+            if 1 <= aNumber && aNumber <= 99
+                then Some (AnyNumber1To99 aNumber)
+                else None
+
+        let CreateAnyNumber1To99FromString (aString:string) =
+            match System.Int32.TryParse(aString) with
+               | (true,int) -> CreateAnyNumber1To99FromInt(int)
+               | _ -> None
+
+        let CreateAnyNumber1To99 (x:obj) = 
+            match x with
+                | :? string as s -> CreateAnyNumber1To99FromString s
+                | :? int as i -> CreateAnyNumber1To99FromInt i
+                | _ -> None
+
+        let Unpack (AnyNumber1To99 i) = i
+        
+        let MultiplyBy5 current = current * 5
+        let Add25 current = current + 25
+        let DivideBy5 current = current / 5
+        let SubtractOriginal (current, original) = current - original
+
+        let Solve (anyNumber1To99 : Option<AnyNumber1To99>) : Option<int> =
+            match anyNumber1To99 with
+                | Some number1To99 ->
+                    number1To99
+                    |> Unpack
+                    |> MultiplyBy5
+                    |> Add25
+                    |> DivideBy5
+                    |> fun x -> (x, Unpack number1To99)
+                    |> SubtractOriginal
+                    |> Some
+                | _ -> None
+        
---
+++ FSharpFramework/FSharpFramework/FSharp.ConsoleApp/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open Always5
+open System
+
+let doTrick1 userInput = 
+    let number = userInput |> int
+    number |> Trick1.Solve |> Some
+
+let doTrick2 number = 
+    let someNumber = Trick2.CreateAnyNumber1To99 number
+    Trick2.Solve someNumber
+
+[<EntryPoint>]
+let main argv = 
+    printfn "Enter a number:"
+    let userInput = Console.ReadLine()
+    let result = doTrick1 userInput
+    match result with
+        | Some num -> printfn "Your result is %d" num
+        | None -> printfn "User Input Is Invalid"
+    let trash = Console.ReadLine()
+    0 // return an integer exit code
